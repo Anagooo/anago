2ffa81ea204c37ae157aeede7ff9a97d0606a1b1
Anagooo <95133777+Anagooo@users.noreply.github.com>
2021-11-29 18:45:16 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -13,8 +13,8 @@ async def on_command_error(ctx, error):
 
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+async def hy(ctx):
+    await ctx.send('こ')
 
 
 token = getenv('DISCORD_BOT_TOKEN')
